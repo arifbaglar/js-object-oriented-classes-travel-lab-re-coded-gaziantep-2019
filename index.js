3c5a93676bd3b31759b087d79f575b0040fa203f
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-14 12:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,25 @@ class Driver{
   }
 }
 class Route{
-  constructor()
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation=beginningLocation;
+    this.endingLocation=endingLocation;
+  }
+  
+
+  blocksTravelled(){
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+      let fark= Math.abs(eastWest.indexOf(this.beginningLocation.horizontal)-eastWest.indexOf(this.endingLocation.horizontal))+Math.abs(this.beginningLocation.vertical-this.endingLocation.vertical)
+      return fark;
+  }
 }
 
 
